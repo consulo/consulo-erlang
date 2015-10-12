8e064a9e296775be1088634a10328b84abb64840
VISTALL <vistall.valeriy@gmail.com>
2015-10-13 00:59:57 +0300

    compilation

--- src/org/intellij/erlang/debugger/xdebug/ErlangDebugRunner.java
+++ src/org/intellij/erlang/debugger/xdebug/ErlangDebugRunner.java
@@ -1,5 +1,10 @@
 package org.intellij.erlang.debugger.xdebug;
 
+import org.intellij.erlang.application.ErlangApplicationConfiguration;
+import org.intellij.erlang.debugger.remote.ErlangRemoteDebugRunConfiguration;
+import org.intellij.erlang.eunit.ErlangUnitRunConfiguration;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.RunProfile;
 import com.intellij.execution.configurations.RunProfileState;
@@ -7,47 +12,43 @@ import com.intellij.execution.executors.DefaultDebugExecutor;
 import com.intellij.execution.runners.ExecutionEnvironment;
 import com.intellij.execution.runners.GenericProgramRunner;
 import com.intellij.execution.ui.RunContentDescriptor;
-import com.intellij.openapi.project.Project;
 import com.intellij.xdebugger.XDebugProcess;
 import com.intellij.xdebugger.XDebugProcessStarter;
 import com.intellij.xdebugger.XDebugSession;
 import com.intellij.xdebugger.XDebuggerManager;
-import org.intellij.erlang.application.ErlangApplicationConfiguration;
-import org.intellij.erlang.debugger.remote.ErlangRemoteDebugRunConfiguration;
-import org.intellij.erlang.eunit.ErlangUnitRunConfiguration;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-public class ErlangDebugRunner extends GenericProgramRunner {
-  public static final String ERLANG_DEBUG_RUNNER_ID = "ErlangDebugRunner";
+public class ErlangDebugRunner extends GenericProgramRunner
+{
+	public static final String ERLANG_DEBUG_RUNNER_ID = "ErlangDebugRunner";
 
-  @Nullable
-  @Override
-  protected RunContentDescriptor doExecute(Project project,
-                                           RunProfileState state,
-                                           RunContentDescriptor contentToReuse,
-                                           final ExecutionEnvironment env) throws ExecutionException {
-    XDebuggerManager xDebuggerManager = XDebuggerManager.getInstance(project);
-    return xDebuggerManager.startSession(this, env, contentToReuse, new XDebugProcessStarter() {
-      @NotNull
-      @Override
-      public XDebugProcess start(@NotNull XDebugSession session) throws ExecutionException {
-        return new ErlangXDebugProcess(session, env);
-      }
-    }).getRunContentDescriptor();
-  }
+	@Nullable
+	@Override
+	protected RunContentDescriptor doExecute(@NotNull RunProfileState state, @NotNull final ExecutionEnvironment env) throws ExecutionException
+	{
+		XDebuggerManager xDebuggerManager = XDebuggerManager.getInstance(env.getProject());
+		return xDebuggerManager.startSession(env, new XDebugProcessStarter()
+		{
+			@NotNull
+			@Override
+			public XDebugProcess start(@NotNull XDebugSession session) throws ExecutionException
+			{
+				return new ErlangXDebugProcess(session, env);
+			}
+		}).getRunContentDescriptor();
+	}
 
-  @NotNull
-  @Override
-  public String getRunnerId() {
-    return ERLANG_DEBUG_RUNNER_ID;
-  }
+	@NotNull
+	@Override
+	public String getRunnerId()
+	{
+		return ERLANG_DEBUG_RUNNER_ID;
+	}
 
-  @Override
-  public boolean canRun(@NotNull String executorId, @NotNull RunProfile profile) {
-    return DefaultDebugExecutor.EXECUTOR_ID.equals(executorId) &&
-      (profile instanceof ErlangApplicationConfiguration ||
-        profile instanceof ErlangUnitRunConfiguration ||
-        profile instanceof ErlangRemoteDebugRunConfiguration);
-  }
+	@Override
+	public boolean canRun(@NotNull String executorId, @NotNull RunProfile profile)
+	{
+		return DefaultDebugExecutor.EXECUTOR_ID.equals(executorId) && (profile instanceof ErlangApplicationConfiguration ||
+				profile instanceof ErlangUnitRunConfiguration ||
+				profile instanceof ErlangRemoteDebugRunConfiguration);
+	}
 }
