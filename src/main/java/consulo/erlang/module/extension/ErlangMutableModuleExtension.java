8e3366cb2e6de83b36d20183a0947f08fdfdf465
VISTALL <vistall.valeriy@gmail.com>
2020-06-03 19:37:20 +0300

    deprecation cleanup

--- src/main/java/consulo/erlang/module/extension/ErlangMutableModuleExtension.java
+++ src/main/java/consulo/erlang/module/extension/ErlangMutableModuleExtension.java
@@ -1,26 +1,32 @@
 package consulo.erlang.module.extension;
 
-import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.openapi.ui.LabeledComponent;
-import com.intellij.openapi.ui.VerticalFlowLayout;
-import com.intellij.openapi.util.text.StringUtil;
-import consulo.extension.ui.ModuleExtensionSdkBoxBuilder;
-import consulo.module.extension.MutableModuleExtensionWithSdk;
-import consulo.module.extension.MutableModuleInheritableNamedPointer;
-import consulo.roots.ModuleRootLayer;
-import consulo.ui.annotation.RequiredUIAccess;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.swing.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.openapi.ui.LabeledComponent;
+import com.intellij.openapi.ui.VerticalFlowLayout;
+import com.intellij.openapi.util.text.StringUtil;
+import consulo.disposer.Disposable;
+import consulo.extension.ui.ModuleExtensionSdkBoxBuilder;
+import consulo.module.extension.MutableModuleExtensionWithSdk;
+import consulo.module.extension.MutableModuleInheritableNamedPointer;
+import consulo.module.extension.swing.SwingMutableModuleExtension;
+import consulo.roots.ModuleRootLayer;
+import consulo.ui.Component;
+import consulo.ui.Label;
+import consulo.ui.annotation.RequiredUIAccess;
+import consulo.ui.layout.VerticalLayout;
 
 /**
  * @author VISTALL
  * @since 08.02.14
  */
-public class ErlangMutableModuleExtension extends ErlangModuleExtension implements MutableModuleExtensionWithSdk<ErlangModuleExtension>
+public class ErlangMutableModuleExtension extends ErlangModuleExtension implements MutableModuleExtensionWithSdk<ErlangModuleExtension>, SwingMutableModuleExtension
 {
 	public ErlangMutableModuleExtension(@Nonnull String id, @Nonnull ModuleRootLayer moduleRootLayer)
 	{
@@ -37,7 +43,15 @@ public class ErlangMutableModuleExtension extends ErlangModuleExtension implemen
 	@RequiredUIAccess
 	@Nullable
 	@Override
-	public JComponent createConfigurablePanel(@Nullable Runnable runnable)
+	public Component createConfigurationComponent(@Nonnull Disposable disposable, @Nonnull Runnable runnable)
+	{
+		return VerticalLayout.create().add(Label.create("Unsupported platform"));
+	}
+
+	@RequiredUIAccess
+	@Nullable
+	@Override
+	public JComponent createConfigurablePanel(@Nonnull Disposable disposable, @Nonnull Runnable runnable)
 	{
 		JPanel panel = new JPanel(new VerticalFlowLayout());
 		panel.add(ModuleExtensionSdkBoxBuilder.createAndDefine(this, runnable).build());
--- src/main/java/consulo/erlang/rebar/module/extension/RebarMutableModuleExtension.java
+++ src/main/java/consulo/erlang/rebar/module/extension/RebarMutableModuleExtension.java
@@ -1,22 +1,27 @@
 package consulo.erlang.rebar.module.extension;
 
-import com.intellij.openapi.projectRoots.Sdk;
-import com.intellij.util.ui.JBUI;
-import consulo.extension.ui.ModuleExtensionSdkBoxBuilder;
-import consulo.module.extension.MutableModuleExtensionWithSdk;
-import consulo.module.extension.MutableModuleInheritableNamedPointer;
-import consulo.roots.ModuleRootLayer;
-import consulo.ui.annotation.RequiredUIAccess;
-
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.swing.*;
+
+import com.intellij.openapi.projectRoots.Sdk;
+import com.intellij.util.ui.JBUI;
+import consulo.disposer.Disposable;
+import consulo.extension.ui.ModuleExtensionSdkBoxBuilder;
+import consulo.module.extension.MutableModuleExtensionWithSdk;
+import consulo.module.extension.MutableModuleInheritableNamedPointer;
+import consulo.module.extension.swing.SwingMutableModuleExtension;
+import consulo.roots.ModuleRootLayer;
+import consulo.ui.Component;
+import consulo.ui.Label;
+import consulo.ui.annotation.RequiredUIAccess;
+import consulo.ui.layout.VerticalLayout;
 
 /**
  * @author VISTALL
  * @since 08.02.14
  */
-public class RebarMutableModuleExtension extends RebarModuleExtension implements MutableModuleExtensionWithSdk<RebarModuleExtension>
+public class RebarMutableModuleExtension extends RebarModuleExtension implements MutableModuleExtensionWithSdk<RebarModuleExtension>, SwingMutableModuleExtension
 {
 	public RebarMutableModuleExtension(@Nonnull String id, @Nonnull ModuleRootLayer moduleRootLayer)
 	{
@@ -33,10 +38,17 @@ public class RebarMutableModuleExtension extends RebarModuleExtension implements
 	@RequiredUIAccess
 	@Nullable
 	@Override
-	public JComponent createConfigurablePanel(@Nullable Runnable runnable)
+	public JComponent createConfigurablePanel(@Nonnull Disposable disposable, @Nonnull Runnable runnable)
 	{
-		return JBUI.Panels.verticalPanel().addComponent(ModuleExtensionSdkBoxBuilder.createAndDefine(this,
-				runnable).build());
+		return JBUI.Panels.verticalPanel().addComponent(ModuleExtensionSdkBoxBuilder.createAndDefine(this, runnable).build());
+	}
+
+	@RequiredUIAccess
+	@Nullable
+	@Override
+	public Component createConfigurationComponent(@Nonnull Disposable disposable, @Nonnull Runnable runnable)
+	{
+		return VerticalLayout.create().add(Label.create("Unsupported platform"));
 	}
 
 	@Override
